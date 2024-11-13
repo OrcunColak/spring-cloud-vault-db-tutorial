f94355721846711e552e2442994d5989d0a498f2
Orçun Çolak <orcun.colak@hazelcast.com>
2024-11-13 10:11:10 +0300

    create table manually

--- docker-compose/vault_db_secrets_setup.sh
+++ docker-compose/vault_db_secrets_setup.sh
@@ -4,6 +4,7 @@
 vault secrets enable database
 
 # Configure database connection
+# allowed_roles : Defines which roles can request credentials for this database.
 vault write database/config/my-postgresql-database \
     plugin_name=postgresql-database-plugin \
     allowed_roles="dynamic-role" \
@@ -12,11 +13,13 @@ vault write database/config/my-postgresql-database \
     password="Simform@123"
 
 # Add dynamic role with expanded permissions
+# creation_statements : Defines SQL statements to create a user and grant permissions. Here, Vault generates credentials and assigns database access based on the role.
+# TTL settings: Set the lifespan of these credentials. default_ttl defines a shorter lifespan, and max_ttl defines the maximum allowed lifespan.
 vault write database/roles/dynamic-role \
     db_name=my-postgresql-database \
     creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; \
                         GRANT CONNECT ON DATABASE \"spring-cloud-vault-demo\" TO \"{{name}}\"; \
-                        GRANT USAGE, CREATE ON SCHEMA public TO \"{{name}}\"; \
+                        GRANT USAGE ON SCHEMA public TO \"{{name}}\"; \
                         GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO \"{{name}}\"; \
                         GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO \"{{name}}\";" \
     default_ttl="1h" \
--- src/main/java/com/colak/springtutorial/jpa/User.java
+++ src/main/java/com/colak/springtutorial/jpa/User.java
@@ -18,6 +18,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class User {
 
     @Id
